dcf20504a1353ea507c0299692279179ee6d09ae
choco1222 <hsh010116@gmail.com>
2026-01-31 00:12:24 +0900

    HiddenCore Folder and Package Test

---
+++ Remember_Spring/src/main/java/JWJ/Test.java
@@ -0,0 +1,11 @@
+package JWJ;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("test");
+		
+	}
+
+}
---
+++ Remember_Spring/src/main/java/KTW/Test.java
@@ -0,0 +1,11 @@
+package KTW;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("Test");
+		
+	}
+
+}
