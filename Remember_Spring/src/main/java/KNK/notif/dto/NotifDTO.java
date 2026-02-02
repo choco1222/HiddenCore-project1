784b568093a01056bd3c5169fa7becb13ad5fc47
nicolekim1248-star <nicolekim1248@gmail.com>
2026-02-02 18:24:16 +0900

    Add files via upload

---
+++ Remember_Spring/src/main/java/KNK/notif/dto/NotifDTO.java
@@ -0,0 +1,29 @@
+package notif.dto;
+
+public class NotifDTO {
+    private String key;
+    private long id;        // ms
+    private String title;
+    private String body;
+    private String type;    // routineType
+
+    public NotifDTO(String key, long id, String title, String body, String type) {
+        this.key = key;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.type = type;
+    }
+
+    public String getKey() { return key; }
+    public long getId() { return id; }
+    public String getTitle() { return title; }
+    public String getBody() { return body; }
+    public String getType() { return type; }
+
+    public void setKey(String key) { this.key = key; }
+    public void setId(long id) { this.id = id; }
+    public void setTitle(String title) { this.title = title; }
+    public void setBody(String body) { this.body = body; }
+    public void setType(String type) { this.type = type; }
+}
