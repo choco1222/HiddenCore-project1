6e81737662b88f0101f6e9a8f3959740655bf8eb
choco1222 <hsh010116@gmail.com>
2026-02-03 11:52:35 +0900

    Remake Package

--- Remember_Spring/src/main/java/JWJ/Remake.java
+++ Remember_Spring/src/main/java/JWJ/Remake.java
@@ -1,11 +1,11 @@
 package JWJ;
 
-public class Test {
+public class Remake {
 
 	public static void main(String[] args) {
 
-		System.out.println("다시올려");
+		System.out.println("Remake Folder");
 		
 	}
-	
+
 }
